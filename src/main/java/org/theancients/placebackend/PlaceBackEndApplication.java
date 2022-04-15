235d49151d94cd3f58b981ac102e0cf7759248a0
minecraft-simon <minecraft_simon@gmx.net>
2022-04-15 16:39:56 +0200

    initial commit

---
+++ src/main/java/org/theancients/placebackend/PlaceBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.theancients.placebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/theancients/placebackend/PlaceBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.theancients.placebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
