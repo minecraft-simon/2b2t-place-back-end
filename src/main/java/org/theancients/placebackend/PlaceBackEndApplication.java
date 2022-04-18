b4e297b99b40994a4576f5a2da0182a603439d6d
minecraft-simon <minecraft_simon@gmx.net>
2022-04-18 03:43:34 +0200

    added bootWar

--- src/main/java/org/theancients/placebackend/PlaceBackEndApplication.java
+++ src/main/java/org/theancients/placebackend/PlaceBackEndApplication.java
@@ -3,14 +3,28 @@ package org.theancients.placebackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class}) // exclude is needed to disable default password
+/*
 @EnableScheduling
+@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class}) // exclude is needed to disable default password
 public class PlaceBackEndApplication {
-
-	public static void main(String[] args) {
+	public static void main(String[] args)
 		SpringApplication.run(PlaceBackEndApplication.class, args);
 	}
+} */
 
+@EnableScheduling
+@SpringBootApplication(exclude={UserDetailsServiceAutoConfiguration.class})
+public class PlaceBackEndApplication extends SpringBootServletInitializer {
+	@Override
+	protected final SpringApplicationBuilder
+	configure(final SpringApplicationBuilder application) {
+		return application.sources(PlaceBackEndApplication.class);
+	}
+	public static void main(final String[] args) throws Exception {
+		SpringApplication.run(PlaceBackEndApplication.class, args);
+	}
 }
